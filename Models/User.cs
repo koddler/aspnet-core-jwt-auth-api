26192261b436f088c653ee01d8e1d2a8b6839e44
Shaphil <shaphil2025@gmail.com>
2019-05-18 18:01:12 +0600

    Promote User.Id from int to long

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,7 @@ namespace AuthApi.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
