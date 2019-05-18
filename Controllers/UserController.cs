24efce7570aed22f5af730d11c0332ac92d3680d
Shaphil <shaphil2025@gmail.com>
2019-05-19 03:30:31 +0600

    Return JWT on successful Login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 using AuthApi.Models;
@@ -54,6 +60,35 @@ namespace AuthApi.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<object>> Login(User user)
+        {
+            var _user = await _context.Users.SingleOrDefaultAsync(
+                u => u.Username == user.Username && u.Password == user.Password
+            );
+
+            if (_user == null)
+                return NotFound();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var secret = Encoding.ASCII.GetBytes("70e396f94d84b205077262ba81143762");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, _user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(5),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(secret),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+            var _token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenKey = tokenHandler.WriteToken(_token);
+
+            return Ok(new { token = tokenKey });
         }
     }
 }
