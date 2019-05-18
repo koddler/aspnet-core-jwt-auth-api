faee8c42fce07121599a4ef319b70445fdd450fa
Shaphil <shaphil2025@gmail.com>
2019-05-19 02:14:14 +0600

    Add Required attribute to Username, Email and Password

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AuthApi.Models
 {
     public class User
     {
         public long Id { get; set; }
+
+        [Required]
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
