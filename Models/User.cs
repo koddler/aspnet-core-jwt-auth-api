225ea33a4cc741f1d0b3ccd4b5c666eec63332dd
Shaphil <shaphil2025@gmail.com>
2019-05-19 03:28:15 +0600

    Remove required attribute from Email

--- Models/User.cs
+++ Models/User.cs
@@ -10,8 +10,6 @@ namespace AuthApi.Models
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
-        [Required]
         public string Email { get; set; }
 
         [Required]
