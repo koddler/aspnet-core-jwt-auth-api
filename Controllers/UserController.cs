b1c309ae3f18893fd1582ef5d99dd4d0e635d287
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-05-20 04:58:47 +0600

    Refactor Controllers
    * Move Login() and Register() to AuthController
    * Refactor UserController api routes

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,12 +2,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,11 +15,11 @@ namespace AuthApi.Controllers
     [Authorize]
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
         private readonly UserContext _context;
 
-        public UserController(UserContext context)
+        public UsersController(UserContext context)
         {
             _context = context;
         }
@@ -51,55 +48,6 @@ namespace AuthApi.Controllers
 
             user.Password = null;
             return user;
-        }
-
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public async Task<ActionResult<User>> Register(User user)
-        {
-            var _user = await _context.Users.SingleOrDefaultAsync(
-                u => u.Username == user.Username
-            );
-
-            if (_user != null)
-            {
-                return BadRequest(new { error = "Username already taken" });
-            }
-
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
-        }
-
-        [AllowAnonymous]
-        [HttpPost("login")]
-        public async Task<ActionResult<object>> Login(User user)
-        {
-            var _user = await _context.Users.SingleOrDefaultAsync(
-                u => u.Username == user.Username && u.Password == user.Password
-            );
-
-            if (_user == null)
-                return NotFound();
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var secret = Encoding.ASCII.GetBytes("70e396f94d84b205077262ba81143762");
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[] {
-                    new Claim(ClaimTypes.Name, _user.Id.ToString())
-                }),
-                Expires = DateTime.UtcNow.AddDays(5),
-                SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(secret),
-                    SecurityAlgorithms.HmacSha256Signature
-                )
-            };
-            var _token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenKey = tokenHandler.WriteToken(_token);
-
-            return Ok(new { token = tokenKey });
         }
     }
 }
